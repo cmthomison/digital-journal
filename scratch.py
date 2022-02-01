1e01f8ae3401be6530bf16768ccd90a7edf6691c
Claire Thomison <cmthomison@gmail.com>
2022-01-31 21:04:34 -0600

    GCP example testing.

--- scratch.py
+++ scratch.py
@@ -8,11 +8,49 @@ from google.cloud import vision
 from google.cloud import bigquery
 
 
+# Modified function to detect text from GCP examples.
+# https://cloud.google.com/vision/docs/ocr#vision_text_detection-python
+def detect_text_uri(uri, gcp_creds):
+    """
+    Detects text in the file located in Google Cloud Storage or on the Web.
+    """
+
+    client = vision.ImageAnnotatorClient(credentials=gcp_creds)
+    image = vision.Image()
+    image.source.image_uri = uri
+
+    response = client.text_detection(image=image)
+    texts = response.text_annotations
+    print('Texts:')
+
+    for text in texts:
+        print('\n"{}"'.format(text.description))
+
+        vertices = (['({},{})'.format(vertex.x, vertex.y)
+                    for vertex in text.bounding_poly.vertices])
+
+        print('bounds: {}'.format(','.join(vertices)))
+
+    if response.error.message:
+        raise Exception(
+            '{}\nFor more info on error messages, check: '
+            'https://cloud.google.com/apis/design/errors'.format(
+                response.error.message))
+
 # Set up GCP cred authentication.
 creds = pydata_google_auth.get_user_credentials(
     ['https://www.googleapis.com/auth/cloud-platform']
 )
 
+# Get list of images in GCS bucket.
+
+
+
+
+
+
+
+# BQ Scratch
 client = bigquery.Client(project='digital-journal-340001',credentials=creds)
 q = """
 SELECT * FROM `digital-journal-340001.test.test`
