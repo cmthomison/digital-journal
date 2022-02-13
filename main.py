b9699dfd102a96eb3d13cd5732b56d80109d844c
Claire Thomison <cmthomison@gmail.com>
2022-02-13 10:12:45 -0600

    Initial build of CF entry point.

--- main.py
+++ main.py
@@ -8,8 +8,27 @@ from google.cloud import storage
 from google.cloud import vision
 from datetime import datetime as dt
 
+from word_retriever import NbPage
+
 
 # Manage credentials based on execution location.
 if os.envrion.get('GCP_PROJECT') is not None:
     os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="/home/clairethomison/creds/djcr.json"
+
+# Cloud Function Entry Point
+def img_to_text(event):
+    """Cloud function triggered on new file in GCS that converts a photo of a 
+    notebook page to text.
+    """
+
+    print(f'File {event.nane} to be processed.')
+
+    # Initialize NbPage class and get text.
+    page = NbPage(event.selfLink)
+    text = NbPage.get_text()
+    page.load_result(text)
+
+    print(f'File {event.name} converted to text and loaded to GCS.')
+
+
 
